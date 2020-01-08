53e4a19aab038fe0a3533d36e4cbd5fdd516b700
matheusmoreira12 <matheusmatos32@gmail.com>
2020-01-08 17:38:34 -0300

    Update BlendMode.ts

--- framework/Source/UserInterface/Colors/Blending/BlendMode.ts
+++ framework/Source/UserInterface/Colors/Blending/BlendMode.ts
@@ -1,18 +1,49 @@
 //Documentation: https://www.w3.org/TR/compositing-1/
 
 import { Color } from "../Color.js";
+import { InvalidOperationException } from "../../../Standard/Exceptions.js";
+import { ColorRGB } from "../ColorRGB.js";
 
-export type BlendModeFunction = (cb: Color, cs: Color) => Color;
+export type SeparableBlendModeFunction = (cb: number, cs: number) => number;
+export type NonSeparableBlendModeFunction = (cb: Color, cs: Color) => Color;
 
-export class BlendMode {
-    constructor(b: BlendModeFunction) {
-        this.__b = b;
+export abstract class BlendMode {
+    constructor() {
+        if (new.target === BlendMode)
+            throw new InvalidOperationException("Invalid constructor.");
     }
 
-    get b(): BlendModeFunction { return this.__b; }
-    private __b: BlendModeFunction;
+    public abstract blend(cb: Color, cs: Color): Color;
 }
 
 export class SeparableBlendMode extends BlendMode {
+    constructor(b: SeparableBlendModeFunction) {
+        super();
 
+        this.__b = b;
+    }
+
+    public blend(cb: Color, cs: Color): Color {
+        const cbRGB = cb.toRGB(),
+            csRGB = cs.toRGB();
+        return new ColorRGB(this.b(cbRGB.r, csRGB.r), this.b(cbRGB.g, csRGB.g), this.b(cbRGB.b, csRGB.b));
+    }
+
+    get b(): SeparableBlendModeFunction { return this.__b; }
+    private __b: SeparableBlendModeFunction;
+}
+
+export class NonSeparableBlendMode extends BlendMode {
+    constructor(b: NonSeparableBlendModeFunction) {
+        super();
+
+        this.__b = b;
+    }
+
+    public blend(cb: Color, cs: Color): Color {
+        return this.b(cb, cs);
+    }
+
+    get b(): NonSeparableBlendModeFunction { return this.__b; }
+    private __b: NonSeparableBlendModeFunction;
 }
