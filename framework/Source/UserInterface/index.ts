b55beae87792b823136ef63b5160237b77896e91
matheusmoreira12 <matheusmatos32@gmail.com>
2020-01-20 14:34:26 -0300

    Overhaul (possibly) complete

--- framework/Source/UserInterface/index.ts
+++ framework/Source/UserInterface/index.ts
@@ -5,9 +5,11 @@ export * from "./DragEmulator.js";
 export * from "./Timer.js";
 export * from "./Animations.js";
 export * from "./AutoScroller.js";
-export * from "./DOMUtils.js";
 export * from "./DragDropHandler.js";
 export * from "./DragEmulator.js";
-export * from "./DOMUtils.js";
-export * from "./DOMUtils.js";
-export * from "./DOMUtils.js";
+export * from "./Timer.js";
+export * from "./VisualTreeManagement.js";
+export * from "./Xaml.js";
+
+import * as DOMUtils from "./DOMUtils.js";
+export { DOMUtils };
