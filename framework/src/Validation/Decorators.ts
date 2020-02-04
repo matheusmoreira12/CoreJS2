bc383e89155d01063ab6ac54336071ae0cabebc2
matheusmoreira12 <matheusmatos32@gmail.com>
2020-02-04 18:20:54 -0300

    Update Decorators.ts

--- framework/src/Validation/Decorators.ts
+++ framework/src/Validation/Decorators.ts
@@ -1,5 +1,10 @@
 import { Type } from "../Standard/Types/index";
 
+export function validate(): MethodDecorator {
+    return function<T> (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>) {
+    };
+}
+
 export function assert(type: any): ParameterDecorator;
 export function assert(type: Type): ParameterDecorator;
 export function assert(types: any[]): ParameterDecorator;
