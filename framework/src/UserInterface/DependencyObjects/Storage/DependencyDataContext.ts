fa64fa0ca190c668c848451e699ca4a32e24c238
matheusmoreira12 <matheusmatos32@gmail.com>
2020-02-18 15:22:41 -0300

    Update DependencyDataContext.ts

--- framework/src/UserInterface/DependencyObjects/Storage/DependencyDataContext.ts
+++ framework/src/UserInterface/DependencyObjects/Storage/DependencyDataContext.ts
@@ -6,6 +6,8 @@ import { assertEachParams, assert } from "../../../Validation/index";
 import { StorageSetter, createStorageSetter } from "./StorageSetter";
 import { PropertyMetadata } from "../PropertyMetadata";
 
+type Target = typeof DependencyObject | DependencyObject;
+
 const $setters = Symbol();
 const $metadata = Symbol();
 
@@ -20,53 +22,27 @@ export class DependencyDataContext extends DataContext {
     }
 
     branchOut(target: typeof DependencyObject | DependencyObject): DependencyDataContext {
-        const branchContext = new DependencyDataContext(target);
-        this.children.add(branchContext);
-        return branchContext;
+        return branchOutContext.call(this, target);
     }
 
     setValue(source: object, property: DependencyProperty, value: any) {
-        let setter = this.setters.find(s => s.source === source && s.property === property);
-        if (setter === undefined) {
-            setter = createStorageSetter(source, property, value);
-            this.setters.add(setter);
-        }
-        else
-            setter.value = value;
+        setValueOnContext.call(this, source, property, value);
     }
 
     unsetValue(source: object, property: DependencyProperty) {
-        const setter = this.setters.find(s => s.source === source && s.property === property);
-        if (setter !== undefined)
-            this.setters.remove(setter);
+        unsetValueOnContext.call(this, source, property);
     }
 
     computeMetadata(property: DependencyProperty): PropertyMetadata | null {
         assert({ property }, DependencyProperty);
 
-        for (let context of this.getTree()) {
-            if (context instanceof DependencyDataContext) {
-                const metadata = context.metadata;
-                for (let metadataEntry of metadata)
-                    if (metadataEntry.property === property)
-                        return metadataEntry;
-            }
-        }
-        return null;
+        return computeMetadataOnContext.call(this, property);
     }
 
     computeValue(property: DependencyProperty): any {
         assert({ property }, DependencyProperty);
 
-        for (let context of this.getTree())
-            if (context instanceof DependencyDataContext) {
-                const reversedSetters = context.setters.reverse();
-                for (let setter of reversedSetters)
-                    if (setter.property === property && setter.value !== DependencyProperty.unsetValue)
-                        return setter.value;
-            }
-
-        return DependencyProperty.unsetValue;
+        return computeValueOnContext.call(this, property);
     }
 
     get setters(): Collection<StorageSetter> { return this[$setters]; }
@@ -74,4 +50,56 @@ export class DependencyDataContext extends DataContext {
 
     get metadata(): Collection<PropertyMetadata> { return this[$metadata]; }
     private [$metadata]: Collection<PropertyMetadata>;
+}
+
+function branchOutContext(this: DependencyDataContext, target: Target): DependencyDataContext {
+    const branchContext = new DependencyDataContext(target);
+    this.children.add(branchContext);
+    return branchContext;
+}
+
+function setValueOnContext(this: DependencyDataContext, source: object, property: DependencyProperty, value: any) {
+    let setter = this.setters.find(s => s.source === source && s.property === property);
+    if (setter === undefined) {
+        setter = createStorageSetter(source, property, value);
+        this.setters.add(setter);
+    }
+    else
+        setter.value = value;
+}
+
+function unsetValueOnContext(this: DependencyDataContext, source: object, property: DependencyProperty) {
+    const setter = this.setters.find(s => s.source === source && s.property === property);
+    if (setter !== undefined)
+        this.setters.remove(setter);
+}
+
+function computeValueOnContext(this: DependencyDataContext, property: DependencyProperty) {
+    const metadata = this.computeMetadata(property);
+
+    for (let context of this.getTree())
+        if (context instanceof DependencyDataContext) {
+            const reversedSetters = context.setters.reverse();
+            for (let setter of reversedSetters)
+                if (setter.property === property && setter.value !== DependencyProperty.unsetValue)
+                    return setter.value;
+        }
+
+    return DependencyProperty.unsetValue;
+}
+
+function overrideMetadataOnContext(this: DependencyDataContext, property: DependencyProperty, metadata: PropertyMetadata) {
+
+}
+
+function computeMetadataOnContext(this: DataContext, property: DependencyProperty): PropertyMetadata | null {
+    for (let context of this.getTree()) {
+        if (context instanceof DependencyDataContext) {
+            const metadata = context.metadata;
+            for (let metadataEntry of metadata)
+                if (metadataEntry.property === property)
+                    return metadataEntry;
+        }
+    }
+    return null;
 }
