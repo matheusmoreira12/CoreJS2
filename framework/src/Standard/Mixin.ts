8817bd5bff015005f9901b865e6c97734d44ef92
matheusmoreira12 <matheusmatos32@gmail.com>
2020-02-19 18:19:24 -0300

    Update Mixin.ts

--- framework/src/Standard/Mixin.ts
+++ framework/src/Standard/Mixin.ts
@@ -3,7 +3,7 @@ import { ObjectUtils } from "../CoreBase/Utils/index";
 const $baseClass = Symbol();
 const $mixinClasses = Symbol();
 
-export class Mixin<TBase, T1, T2 = undefined, T3 = undefined, T4 = undefined, T5 = undefined, T6 = undefined, T7 = undefined> {
+export class Mixin<TBase, T1, T2 = never, T3 = never, T4 = never, T5 = never, T6 = never, T7 = never> extends Function {
     static create<TBase, T1>(baseClass: TBase, class1: T1): Mixin<TBase, T1> & TBase & T1;
     static create<TBase, T1, T2>(baseClass: TBase, class1: T1, class2: T2): Mixin<TBase, T1, T2> & TBase & T1 & T2;
     static create<TBase, T1, T2, T3>(baseClass: TBase, class1: T1, class2: T2, class3: T3): Mixin<TBase, T1, T2, T3> & TBase & T1 & T2 & T3;
@@ -11,14 +11,11 @@ export class Mixin<TBase, T1, T2 = undefined, T3 = undefined, T4 = undefined, T5
     static create<TBase, T1, T2, T3, T4, T5>(baseClass: TBase, class1: T1, class2: T2, class3: T3, class4: T4, class5: T5): Mixin<TBase, T1, T2, T3, T4, T5> & TBase & T1 & T2 & T3 & T4 & T5;
     static create<TBase, T1, T2, T3, T4, T5, T6>(baseClass: TBase, class1: T1, class2: T2, class3: T3, class4: T4, class5: T5, class6: T6): Mixin<TBase, T1, T2, T3, T4, T5, T6> & TBase & T1 & T2 & T3 & T4 & T5 & T6;
     static create<TBase, T1, T2, T3, T4, T5, T6, T7>(baseClass: TBase, class1: T1, class2: T2, class3: T3, class4: T4, class5: T5, class6: T6, class7: T7): Mixin<TBase, T1, T2, T3, T4, T5, T6, T7> & TBase & T1 & T2 & T3 & T4 & T5 & T6 & T7;
-    static create<TBase, T1, T2 = undefined, T3 = undefined, T4 = undefined, T5 = undefined, T6 = undefined, T7 = undefined>(baseClass: TBase, ...mixinClasses: (T1 | T2 | T3 | T4 | T5 | T6 | T7)[]): Mixin<TBase, T1, T2, T3, T4, T5, T6, T7> & TBase & T1 & T2 & T3 & T4 & T5 & T6 & T7 {
+    static create<TBase, T1, T2 = never, T3 = never, T4 = never, T5 = never, T6 = never, T7 = never>(baseClass: TBase, ...mixinClasses: (T1 | T2 | T3 | T4 | T5 | T6 | T7)[]): Mixin<TBase, T1, T2, T3, T4, T5, T6, T7> & TBase & T1 & T2 & T3 & T4 & T5 & T6 & T7 {
         const result = new Mixin();
         applyMixins(result, baseClass, mixinClasses);
 
         return <any>result;
-    }
-
-    constructor() {
     }
 
     get baseClass(): TBase { return this[$baseClass]; }
@@ -28,8 +25,8 @@ export class Mixin<TBase, T1, T2 = undefined, T3 = undefined, T4 = undefined, T5
     private [$mixinClasses]: (T1 | T2 | T3 | T4 | T5 | T6 | T7)[];
 }
 
-function applyMixins(result: object, baseClass: any, mixinClasses: any[]) {
-    ObjectUtils.crudeCopy(baseClass, result);
+function applyMixins(result: any, baseClass: any, mixinClasses: any[]) {
+    ObjectUtils.crudeCopy(baseClass.prototype, result.prototype);
     for (let mixinClass of mixinClasses)
-        ObjectUtils.crudeCopy(mixinClass, result, false);
+        ObjectUtils.crudeCopy(mixinClass.prototype, result.prototype, false);
 }
