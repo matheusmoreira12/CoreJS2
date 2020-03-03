b17a76c0dfc87049e51b667aa6f331a495ba8bcf
matheusmoreira12 <matheusmatos32@gmail.com>
2020-03-03 15:59:54 -0300

    ...

--- framework/src/UserInterface/DependencyObjects/DependencyObject.ts
+++ framework/src/UserInterface/DependencyObjects/DependencyObject.ts
@@ -2,46 +2,39 @@ import { DependencyProperty } from "./DependencyProperty";
 import { FrameworkEvent } from "../../Standard/Events/index";
 import { PropertyChangeEventArgs } from "./PropertyChangeEvent";
 import { DataContext } from "../DataContexts/index";
-import { InvalidOperationException } from "../../Standard/index";
+import { InvalidOperationException, InvalidTypeException } from "../../Standard/index";
+import { assertParams } from "../../Validation/index";
 import { DataContexts } from "../index";
 import { DependencyDataContext } from "./Storage/index";
-import { Interface, InterfaceMember, InterfaceMemberType } from "../../Standard/Interfaces/index";
-import { Type } from "../../Standard/Types/index";
-import { assertParams } from "../../Validation/index";
 
-export const IDependencyObject = new Interface(
-    new InterfaceMember("get", InterfaceMemberType.Function),
-    new InterfaceMember("set", InterfaceMemberType.Function),
-    new InterfaceMember("PropertyChangeEvent", InterfaceMemberType.Property, Type.get(FrameworkEvent)),
-);
+export class DependencyObject {
+    get(property: DependencyProperty): any {
+        assertParams({ property }, DependencyProperty);
 
-export interface IDependencyObject {
-    get(property: DependencyProperty): any;
-    set(property: DependencyProperty, value: any): void;
-    PropertyChangeEvent: FrameworkEvent<PropertyChangeEventArgs>;
+        return getPropertyValue(this, property);
+    }
+
+    set(property: DependencyProperty, value: any) {
+        assertParams({ property }, DependencyProperty);
+
+        setPropertyValue(this, property, value);
+    }
+
+    PropertyChangeEvent: FrameworkEvent<PropertyChangeEventArgs> = new FrameworkEvent();
 }
 
-export const DependencyObject = {
+function getPropertyValue(target: DependencyObject, property: DependencyProperty) {
+    const context = DataContexts.Utils.getNearestByConstructor(DataContext.main, target) as DependencyDataContext | null;
+    if (context === null)
+        throw new InvalidOperationException("Cannot get property value. No dependency data context corresponds to the provided target.");
+    else
+        context.computeValue(property, target);
+}
 
-    getPropertyValue(target: IDependencyObject, property: DependencyProperty): any {
-        assertParams({ target }, IDependencyObject);
-        assertParams({ property }, DependencyProperty);
-
-        const context = DataContexts.Utils.getNearestByConstructor(DataContext.main, target) as DependencyDataContext | null;
-        if (context === null)
-            throw new InvalidOperationException("Cannot get property value. No dependency data context corresponds to the provided target.");
-        else
-            return context.computeValue(property, target);
-    },
-
-    setPropertyValue(target: IDependencyObject, property: DependencyProperty, value: any) {
-        assertParams({ target }, IDependencyObject);
-        assertParams({ property }, DependencyProperty);
-
-        const context = DataContexts.Utils.getNearestByConstructor(DataContext.main, target) as DependencyDataContext | null;
-        if (context === null)
-            throw new InvalidOperationException("Cannot set property value. No dependency data context corresponds to the provided target.");
-        else
-            context.setValue(target, target, property, value);
-    }
+function setPropertyValue(target: DependencyObject, property: DependencyProperty, value: any): any {
+    const context = DataContexts.Utils.getNearestByConstructor(DataContext.main, target) as DependencyDataContext | null;
+    if (context === null)
+        throw new InvalidOperationException("Cannot set property value. No dependency data context corresponds to the provided target.");
+    else
+        context.setValue(target, target, property, value);
 }
