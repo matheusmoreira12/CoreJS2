a3d3679c2f9dd521b6310e24bbe73eb32136e02a
matheusmoreira12 <matheusmatos32@gmail.com>
2020-03-09 18:41:06 -0300

    Update PropertyAttributeBinding.ts
    Implementation in progress

--- framework/src/UserInterface/Bindings/PropertyAttributeBinding.ts
+++ framework/src/UserInterface/Bindings/PropertyAttributeBinding.ts
@@ -1,31 +1,41 @@
 import { Binding, IBindingOptions } from "./index";
-import { DependencyProperty } from "../DependencyObjects/index";
+import { DependencyProperty, PropertyChangeEventArgs } from "../DependencyObjects/index";
 import { assertParams } from "../../Validation/index";
 import { DependencyObject } from "../DependencyObjects/DependencyObject";
+import { FrameworkEvent, FrameworkEventArgs } from "../../Standard/Events/index";
 
 //Keys for PropertyAttributeBinding
 const $source = Symbol();
 const $sourceProperty = Symbol();
 const $targetElement = Symbol();
 const $targetAttributeName = Symbol();
+const $targetAttributeNamespace = Symbol();
 
 /**
  * PropertyAttributeBinding class
  * Allows the binding of an attribute to a framework property.
  */
 export class PropertyAttributeBinding extends Binding {
-    constructor(source: DependencyObject, sourceProperty: DependencyProperty, targetElement: Element, targetAttributeName: string, options?: IBindingOptions) {
+    constructor(source: DependencyObject, sourceProperty: DependencyProperty, targetElement: Element, targetAttributeName: string, targetAttributeNamespace: string | null = null, options?: IBindingOptions) {
         super(options);
 
         assertParams({ source }, DependencyObject);
         assertParams({ sourceProperty }, DependencyProperty);
         assertParams({ targetElement }, Element);
         assertParams({ targetAttributeName }, String);
+        assertParams({ targetAttributeNamespace }, String, null);
 
         this[$source] = source;
         this[$sourceProperty] = sourceProperty;
         this[$targetElement] = targetElement;
         this[$targetAttributeName] = targetAttributeName;
+        this[$targetAttributeNamespace] = targetAttributeNamespace;
+
+        this.__source_PropertyChangeEvent = new FrameworkEvent(this.__source_onPropertyChange, this);
+        source.PropertyChangeEvent.attach(this.__source_PropertyChangeEvent);
+
+        this.__targetElement_attributeMutationObserver = new MutationObserver(this.__targetElement_attributeChange_handler.bind(this));
+        this.__targetElement_attributeMutationObserver.observe(targetElement);
     }
 
     get source(): DependencyObject { return this[$source]; }
@@ -40,6 +50,35 @@ export class PropertyAttributeBinding extends Binding {
     get targetAttributeName(): string { return this[$targetAttributeName]; }
     private [$targetAttributeName]: string;
 
+    get  targetAttributeNamespace(): string | null { return this[$targetAttributeNamespace]; }
+    private [$targetAttributeNamespace]: string | null;
+
+    private __updateTargetAttribute(property: DependencyProperty, newValue: any) {
+
+    }
+
+    private __source_onPropertyChange(sender: any, args: PropertyChangeEventArgs) {
+        if (args.property === this.sourceProperty)
+            this.__updateTargetAttribute(args.property, args.newValue);
+    }
+
+    private __source_PropertyChangeEvent: FrameworkEvent<PropertyChangeEventArgs>;
+
+    private __updateSourceProperty() {
+
+    }
+
+    private __targetElement_attributeChange_handler(mutations: MutationRecord[]) {
+        for (let mutation of mutations) {
+            if (mutation.type == "attributes" && mutation.attributeName == this.targetAttributeName && mutation.attributeNamespace == this.targetAttributeNamespace)
+                this.__updateSourceProperty();
+        }
+    }
+
+    private __targetElement_attributeMutationObserver: MutationObserver;
+
     protected destructor(): void {
+        this.__source_PropertyChangeEvent.destruct();
+        this.__targetElement_attributeMutationObserver.disconnect();
     }
 }
