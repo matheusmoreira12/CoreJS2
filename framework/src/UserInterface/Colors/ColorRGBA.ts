221334caf419a9e3045901beb9fac901984c2232
MMM <matheusmatos32@gmail.com>
2021-03-22 13:14:35 -0300

    ColorRGBA Corrections / parsing implementation

--- framework/src/UserInterface/Colors/ColorRGBA.ts
+++ framework/src/UserInterface/Colors/ColorRGBA.ts
@@ -6,20 +6,51 @@ import { ColorConversion } from "./index.js";
 import { Color } from "./index.js";
 
 export class ColorRGBA extends Color {
-    constructor(r: number, g: number, b: number, a: number) {
-        const value = ColorConversion.convertFromRGBA(r, g, b, a);
+    constructor(r: ColorChannel, g: ColorChannel, b: ColorChannel, a: ColorChannel) {
+        assertParams({ r }, [ColorChannel]);
+        assertParams({ g }, [ColorChannel]);
+        assertParams({ b }, [ColorChannel]);
+        assertParams({ a }, [ColorChannel]);
+
+        const value = ColorConversion.convertFromRGBA(Number(r), Number(g), Number(b), Number(a));
         super(value);
 
-        this.__r = new ColorChannel(r);
-        this.__g = new ColorChannel(g);
-        this.__b = new ColorChannel(b);
-        this.__a = new ColorChannel(a);
+        this.__r = r;
+        this.__g = g;
+        this.__b = b;
+        this.__a = a;
     }
 
-    static tryParse(value: string, output: TryOutput<ColorRGBA>): boolean {
+    static tryParse(value: string, output: TryOutput<ColorRGBA> = {}): boolean {
         assertParams({ value }, [ String ]);
 
-        
+        if (value.startsWith("rgb(") && value.endsWith(")")) {
+            const channelsStr = value.slice(3, -1); // Remove name and braces
+            const channelStrs = channelsStr.split(","); //Split into the individual channels
+            if (channelStrs.length == 3) {
+                const redStr = channelStrs[0].trim(),
+                    greenStr = channelStrs[1].trim(),
+                    blueStr = channelStrs[2].trim(),
+                    alphaStr = channelStrs[3].trim();
+                
+                const tryParseRedOutput: TryOutput<ColorChannel> = {},
+                    tryParseGreenOutput: TryOutput<ColorChannel> = {},
+                    tryParseBlueOutput: TryOutput<ColorChannel> = {},
+                    tryParseAlphaOutput: TryOutput<ColorChannel> = {};
+
+                const isValid = ColorChannel.tryParse(redStr, tryParseRedOutput) && // Parse red channel
+                    ColorChannel.tryParse(greenStr, tryParseGreenOutput) && // Parse green channel
+                    ColorChannel.tryParse(blueStr, tryParseBlueOutput) && // Parse blue channel
+                    ColorChannel.tryParse(alphaStr, tryParseAlphaOutput); // Parse opacity channel
+
+                output.result = new ColorRGBA(tryParseRedOutput.result, 
+                    tryParseGreenOutput.result, 
+                    tryParseBlueOutput.result, 
+                    tryParseAlphaOutput.result);
+
+                return isValid;
+            }
+        }
     }
 
     static parse(value: string) {
