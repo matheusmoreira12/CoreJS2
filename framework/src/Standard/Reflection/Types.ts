408f832e32e37aedea9909b85dff0be36a4cb78b
MMM <matheusmatos32@gmail.com>
2021-11-03 17:33:13 -0300

    Update Types.ts

--- framework/src/Standard/Reflection/Types.ts
+++ framework/src/Standard/Reflection/Types.ts
@@ -5,7 +5,7 @@ export type ClassOf<T extends object> = ConstructorOf<T> & {
     constructor: Function
 };
 
-export type Method<TArgs extends any[] = [], TResult = void, TThisArg = undefined> = (this: TThisArg, ...args: TArgs) => TResult;
+export type Method<TArgs extends any[] = [], TResult = void, TThisArg = void> = (this: TThisArg, ...args: TArgs) => TResult;
 
 export type InstanceOf<TClass extends ClassOf<Object>> = Object & TClass["prototype"];
 
