0a04fbe7369458c064d3bb3b41906f13641ed0ff
MMM <matheusmatos32@gmail.com>
2021-11-04 00:07:52 -0300

    Renames types.d.ts / Adds primitives

---
+++ framework/src/standard/reflection/types.d.ts
@@ -0,0 +1,22 @@
+import { Interface } from "../interfaces/index.js";
+import { Type } from "./index.js";
+import { TypeConstraint } from "./type-constraints/index.js";
+
+export type Primitive = null | undefined | boolean | number | string | symbol;
+
+export type PrimitiveOrComplex = Primitive | object;
+
+export type ConstructorOf<T extends Object> = T["constructor"];
+
+export type ClassOf<T extends object> = ConstructorOf<T> & {
+    prototype: T,
+    constructor: Function
+};
+
+export type Method<TArgs extends any[] = [], TResult = void, TThisArg = void> = (this: TThisArg, ...args: TArgs) => TResult;
+
+export type InstanceOf<TClass extends ClassOf<Object>> = Object & TClass["prototype"];
+
+export type TryOutput<TResult> = { result?: TResult };
+
+export type TypeMatchingConstraint = Type<any> | Interface | TypeConstraint;
