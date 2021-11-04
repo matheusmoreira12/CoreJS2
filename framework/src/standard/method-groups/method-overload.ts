1883dcc02723aba2e0d9fe98c9de8c8e00a4c6de
MMM <matheusmatos32@gmail.com>
2021-11-04 00:12:49 -0300

    Adds detailed info to MethodOverload

--- framework/src/standard/method-groups/method-overload.ts
+++ framework/src/standard/method-groups/method-overload.ts
@@ -1,10 +1,10 @@
-import { Method } from "../reflection/types";
+import { Method } from "../reflection/index.js";
 import { MethodOverloadParam } from "./method-overload-param.js";
 
 const $params = Symbol("params");
 const $body = Symbol("body");
 
-export class MethodOverload<TOverload extends Method> {
+export class MethodOverload<TParams extends MethodOverloadParam[], TBody extends Method<any[], any, any>> {
     get params(): MethodOverloadParam[]{ return this[$params]; }
 
     private [$params]: MethodOverloadParam[];
