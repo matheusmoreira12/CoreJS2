da5745d101f80cf0e111113860d7d18f005c9294
MMM <matheusmatos32@gmail.com>
2021-11-06 21:23:33 -0300

    Update types.d.ts

--- framework/src/standard/reflection/types.d.ts
+++ framework/src/standard/reflection/types.d.ts
@@ -2,7 +2,7 @@ import { Interface } from "../interfaces/index.js";
 import { Type } from "./index.js";
 import { TypeConstraint } from "./type-constraints/index.js";
 
-export type Primitive = null | undefined | boolean | number | string | symbol;
+export type Primitive = null | undefined | boolean | number | string | bigint | symbol;
 
 type PrimitiveOrComplex = Primitive | object;
 
@@ -14,11 +14,11 @@ type GenericInstance<TClass = any> = { "constructor": NewableFunction & TClass }
 
 export type ConstructorOf<T extends GenericClass | GenericInstance | null | undefined> = T extends (null | undefined) ? never : T extends (GenericClass | GenericInstance) ? T["constructor"] : never;
 
-type ClassOfPrimitive<TPrimitive extends Primitive> = TPrimitive extends null ? null : TPrimitive extends typeof undefined ? undefined : TPrimitive extends boolean ? typeof Boolean : TPrimitive extends number ? typeof Number : TPrimitive extends string ? typeof String : TPrimitive extends symbol ? typeof Symbol : never;
+type ClassOfPrimitive<TPrimitive extends Primitive> = TPrimitive extends null ? null : TPrimitive extends typeof undefined ? undefined : TPrimitive extends boolean ? typeof Boolean : TPrimitive extends number ? typeof Number : TPrimitive extends string ? typeof String : TPrimitive extends symbol ? TPrimitive extends bigint ? typeof BigInt : typeof Symbol : never;
 
 export type ClassOf<TInstance extends GenericInstance | Primitive> = TInstance extends Primitive ? ClassOfPrimitive<TInstance> : TInstance extends GenericInstance ? ConstructorOf<TInstance> : never;
 
-type PrimitiveOfClass<TClass extends ClassOfPrimitive<Primitive>> = TClass extends null ? null : TClass extends typeof undefined ? undefined : TClass extends typeof Boolean ? boolean : TClass extends typeof Number ? number : TClass extends typeof String ? string : TClass extends typeof Symbol ? symbol : never;
+type PrimitiveOfClass<TClass extends ClassOfPrimitive<Primitive>> = TClass extends null ? null : TClass extends typeof undefined ? undefined : TClass extends typeof Boolean ? boolean : TClass extends typeof Number ? number : TClass extends typeof String ? string : TClass extends typeof BigInt ? bigint : TClass extends typeof Symbol ? symbol : never;
 
 export type InstanceOf<TClass extends GenericClass<any> | ClassOfPrimitive<Primitive>> = TClass extends ClassOfPrimitive<Primitive> ? PrimitiveOfClass<TClass> : TClass extends GenericClass<any> ? TClass["prototype"] : never;
 
