999ff8cc938be4785e2161a8371f2e53b16e4ec5
MMM <matheusmatos32@gmail.com>
2022-09-02 22:43:46 -0300

    ZippedEnumerable refactoring

--- framework/src/standard/collections/zipped-enumerable-value.ts
+++ framework/src/standard/collections/zipped-enumerable-value.ts
@@ -1,6 +1,6 @@
 import { IEnumerable } from "./index.js";
 
 type UnwrapEnumerableValue<TEnumerable> = TEnumerable extends IEnumerable<infer T> ? T : TEnumerable;
-type UnwrapEnumerablesValues<TEnumerables extends any[]> = TEnumerables extends [infer THead, ...infer TTail] ? [UnwrapEnumerableValue<THead>, ...UnwrapEnumerablesValues<TTail>] : [];
+type UnwrapEnumerablesValues<TEnumerables> = TEnumerables extends [infer THead, ...infer TTail] ? [UnwrapEnumerableValue<THead>, ...UnwrapEnumerablesValues<TTail>] : [];
 
 export type ZippedEnumerableValue<TSourceEnumerables extends IEnumerable[]> = UnwrapEnumerablesValues<TSourceEnumerables>;
--- framework/src/standard/collections/zipped-enumerator-value.ts
+++ framework/src/standard/collections/zipped-enumerator-value.ts
@@ -1,6 +1,6 @@
 import { IEnumerator } from "./index.js";
 
 type UnwrapEnumeratorValue<TEnumerator> = TEnumerator extends IEnumerator<infer T> ? T : TEnumerator;
-type UnwrapEnumeratorsValues<TEnumerables extends any[]> = TEnumerables extends [infer THead, ...infer TTail] ? [UnwrapEnumeratorValue<THead>, ...UnwrapEnumeratorsValues<TTail>] : [];
+type UnwrapEnumeratorsValues<TEnumerables> = TEnumerables extends [infer THead, ...infer TTail] ? [UnwrapEnumeratorValue<THead>, ...UnwrapEnumeratorsValues<TTail>] : [];
 
 export type ZippedEnumeratorValue<TSourceEnumerators extends IEnumerator[]> = UnwrapEnumeratorsValues<TSourceEnumerators>;
