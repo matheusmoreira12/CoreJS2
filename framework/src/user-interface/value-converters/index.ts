40995467ecd5ea0b9b38916b4c6d319b39df327a
MMM <matheusmatos32@gmail.com>
2022-09-02 23:51:20 -0300

    Corrected export error

--- framework/src/user-interface/value-converters/index.ts
+++ framework/src/user-interface/value-converters/index.ts
@@ -2,5 +2,5 @@ export * from "./i-value-converter.js";
 export * from "./boolean-attribute-value-converter.js";
 export * from "./enumeration-attribute-value-converter.js";
 export * from "./flags-attribute-value-converter.js";
-export * from "./json-attribute-value-converter";
+export * from "./json-attribute-value-converter.js";
 export * from "./mapped-value-converter.js";
