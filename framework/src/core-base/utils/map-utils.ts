3d8d449f2265234d9eac69828f700465282f96c8
MMM <matheusmatos32@gmail.com>
2022-09-04 22:07:22 -0300

    Added methods to utils

--- framework/src/core-base/utils/map-utils.ts
+++ framework/src/core-base/utils/map-utils.ts
@@ -7,4 +7,17 @@ export namespace MapUtils {
 
         return new Map<TValue, TKey>(generateInvertedEntries());
     }
+
+    export function* select<TKey, TValue, TResult>(value: Map<TKey, TValue>, predicate: (key: TKey, value: TValue) => TResult): IterableIterator<TResult> {
+        for (let item of value)
+            yield predicate(item[0], item[1]);
+    }
+
+    export function selectSet<TKey, TValue, TResult>(value: Map<TKey, TValue>, predicate: (key: TKey, value: TValue) => TResult): Set<TResult> {
+        return new Set<TResult>(select(value, predicate));
+    }
+
+    export function* selectMap<TKey, TValue, TResultKey, TResultValue>(value: Map<TKey, TValue>, predicate: (key: TKey, value: TValue) => [TResultKey, TResultValue]): IterableIterator<Map<TResultKey, TResultValue>> {
+        return new Map<TResultKey, TResultValue>(select(value, predicate));
+    }
 }
