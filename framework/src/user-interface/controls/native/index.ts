1a2d761cebedb5c4bb1e1be2eb7acecdcd79aae5
MMM <matheusmatos32@gmail.com>
2022-09-07 01:42:58 -0300

    More exclusions and constant

--- framework/src/user-interface/controls/native/index.ts
+++ framework/src/user-interface/controls/native/index.ts
@@ -1,18 +1,22 @@
 import { DependencyProperty } from "../../../standard/dependency-objects/dependency-property.js";
 import { Control } from "../index.js";
 
-type AllElements = { [K in Exclude<keyof (typeof globalThis), "Element" | "HTMLElement" | "SVGElement" | "MathMLElement"> as (typeof globalThis)[K] extends (typeof Element) ? K : never]: (typeof globalThis)[K] };
+type ElementCtorNameExclusions = typeof ELEMENT_CTOR_NAME_EXCLUSIONS[number];
+
+type AllElements = { [K in Exclude<keyof (typeof globalThis), ElementCtorNameExclusions> as (typeof globalThis)[K] extends (typeof Element) ? K : never]: (typeof globalThis)[K] };
 
 type AllNativeControls = { [K in keyof AllElements]: NativeControlCtor<AllElements[K]> };
 
 type NativeControl<T extends typeof Element> = Control & { [K in keyof T["prototype"] & string]: T["prototype"][K] };
 type NativeControlCtor<T extends typeof Element> = { new(): NativeControl<T>, prototype: NativeControl<T> } & { [K in keyof T["prototype"] & string as `__${K}_property`]: DependencyProperty };
 
+const ELEMENT_CTOR_NAME_EXCLUSIONS = ["Element", "HTMLElement", "SVGElement", "MathMLElement", "Audio", "Source", "Image", "Option", "opener"] as const;
+
 const HTML_NAMESPACE_URI = "http://www.w3.org/1999/xhtml";
 const SVG_NAMESPACE_URI = "http://www.w3.org/2000/svg";
 
-const ELEMENT_CONSTRUCTOR_NAME_MAP: Map<typeof Element, { elementName: string, elementNamespaceURI?: string }> = new Map([
-    // [Element, { elementName: "" }]
+const ELEMENT_CONSTRUCTOR_NAME_MAP: Map<keyof AllElements, { elementName: string, elementNamespaceURI?: string }> = new Map([
+    // ["HTMLHtmlElement", { elementName: "html" }]
 ]);
 
 export const Native: AllNativeControls = generateAllNativeControls();
