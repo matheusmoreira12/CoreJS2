459a3dd511b1ad44504f5a5119ebfd738e0b4c04
Matheus Moreira <matheusmatos32@gmail.com>
2023-02-03 18:29:28 -0300

    Simplifies destructible

--- framework/src/standard/destructible.ts
+++ framework/src/standard/destructible.ts
@@ -2,47 +2,40 @@ import { ObjectUtils } from "../core-base/utils/index.js";
 import { assert } from "../validation-standalone/index.js";
 import { InvalidOperationException } from "./exceptions/index.js";
 
-const allDestructibles: Destructible[] = [];
+const aliveDestructibles: Set<Destructible> = new Set();
 
 export abstract class Destructible {
     constructor() {
         if (new.target === Destructible)
             throw new InvalidOperationException("Invalid constructor.");
 
-        allDestructibles.push(this);
+        aliveDestructibles.add(this);
     }
 
     protected _destructor(): void { }
-
-    destruct() {
-        if (this.__isDestructed)
-            throw new InvalidOperationException("Object has already been destructed.");
-
-        callDestructorsRecursively(this);
-        destructAllDestructibleProperties(this);
-
-        this.__isDestructed = true;
-    }
-
-    get isDestructed(): boolean { return this.__isDestructed; }
-    private __isDestructed: boolean = false;
 }
 
-function callDestructorsRecursively(self: Destructible) {
-    for (let proto of ObjectUtils.getPrototypeTree(self)) {
-        if (proto instanceof Destructible) {
-            assert({ "self._destructor": self["_destructor"] }, [Function]);
-            proto["_destructor"].call(self);
+function destructAlive() {
+    for (let destructible of aliveDestructibles)
+        callDestructorsRecursively(destructible);
+
+    for (let destructible of aliveDestructibles)
+        destructProperties(destructible);
+
+    aliveDestructibles.clear();
+
+    function callDestructorsRecursively(destructible: Destructible) {
+        for (let proto of ObjectUtils.getPrototypeTree(destructible)) {
+            if (proto instanceof Destructible) {
+                assert({ "self._destructor": destructible["_destructor"] }, [Function]);
+                proto["_destructor"].call(destructible);
+            }
         }
     }
-}
 
-function destructAllDestructibleProperties(self: Destructible) {
-    for (let prop of ObjectUtils.getOwnPropertyKeys(self)) {
-        const value = self[prop];
-        if (value instanceof Destructible) {
-            if (!value.isDestructed)
-                value.destruct();
+    function destructProperties(destructible: Destructible) {
+        for (let prop of ObjectUtils.getAllPropertyKeys(destructible)) {
+            Reflect.deleteProperty(destructible, prop);
         }
     }
 }
@@ -51,9 +44,5 @@ window.addEventListener("beforeunload", window_beforeunload);
 
 function window_beforeunload() {
     window.removeEventListener("beforeunload", window_beforeunload);
-
-    for (let destructible of allDestructibles) {
-        if (!destructible.isDestructed)
-            destructible.destruct();
-    }
+    destructAlive();
 }
