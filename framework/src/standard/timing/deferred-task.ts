9dc07f4867e04d0f6db81e89d6c24876a33a7b46
Matheus Moreira <matheusmatos32@gmail.com>
2023-02-07 15:32:34 -0300

    Refactored and enhanced deferred task * Added abortion * Added promise

--- framework/src/standard/timing/deferred-task.ts
+++ framework/src/standard/timing/deferred-task.ts
@@ -1,31 +1,59 @@
+import { assert } from "../../validation/index.js";
 import { Destructible } from "../destructible.js";
 import { Method } from "../reflection/types.js";
 
-export class DeferredTask<TArgs extends any[] = undefined[]> extends Destructible {
+export class DeferredTask<TArgs extends unknown[] = [], TResult extends unknown = void> extends Destructible {
+    constructor(taskCallback: Method<TArgs, TResult>) {
+        assert({ taskCallback }, [Function]);
 
-    constructor(taskCallback: Method<TArgs, void, undefined>) {
         super();
 
-        this.__taskCallback = taskCallback;
+        this.#taskCallback = taskCallback;
     }
 
-    trigger(...args: TArgs): void {
-        this.__cancel();
+    trigger(...args: TArgs): Promise<TResult> {
+        this.abort();
+        this.#isTriggered = true;
+        this.#args = args;
+        return new Promise((resolve, reject) => {
+            this.#promiseResolve = resolve;
+            this.#promiseReject = reject;
+            this.#doTrigger();
+        });
+    }
 
-        this.__immediateHandle = setTimeout(() => {
-            this.__taskCallback(...args);
+    #doTrigger() {
+        this.#immediateHandle = setTimeout(() => {
+            const result = this.#taskCallback.apply(undefined, this.#args!);
+            this.#promiseResolve!(result);
         }, 0);
     }
 
-    private __cancel() {
-        if (this.__immediateHandle != -1)
-            clearTimeout(this.__immediateHandle);
+    abort() {
+        if (!this.#isTriggered)
+            return;
+        this.#doAbort();
+        this.#promiseReject = null;
+        this.#promiseResolve = null;
+        this.#args = null;
+        this.#isTriggered = false;
     }
 
-    private __taskCallback: Function;
-    private __immediateHandle: number = -1;
+    #doAbort() {
+        clearTimeout(this.#immediateHandle!);
+        this.#promiseReject!(null);
+    }
+
+    #taskCallback: Method<TArgs, TResult>;
+    #promiseReject: ((reason?: any) => void) | null = null;
+    #promiseResolve: ((result: TResult | PromiseLike<TResult>) => void) | null = null;
+    #args: TArgs | null = null;
+    #immediateHandle: number | null = 0;
+
+    get isTriggered() { return this.#isTriggered; }
+    #isTriggered: boolean = false;
 
     protected override _destructor() {
-        this.__cancel();
+        this.abort();
     }
 }
