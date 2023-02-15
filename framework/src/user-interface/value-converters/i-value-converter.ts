a14dabb8488ac6d5f8ac92988a58af29e5886de1
Matheus Moreira <matheusmatos32@gmail.com>
2023-02-15 18:19:57 -0300

    Fixes IValueConverter interface

--- framework/src/user-interface/value-converters/i-value-converter.ts
+++ framework/src/user-interface/value-converters/i-value-converter.ts
@@ -12,6 +12,6 @@ export const IValueConverter = new Interface(
  * IValueConverter Interface
  * Exposes a friendly interface for converting values between inner data types and the interface.*/
 export interface IValueConverter {
-    convert: (value: any) => string | null,
-    convertBack: (value: string | null) => any
+    convert: (value: any) => any,
+    convertBack: (value: any) => any
 }
